c451e1b40fc06cffb66a1f274258641ab3d53b44
Sreeganesh Siva <SR33G33@users.noreply.github.com>
2020-06-28 15:35:05 -0700

    changed breeding probability of bacteria to be more accurate

--- src/Bacteria.java
+++ src/Bacteria.java
@@ -11,7 +11,7 @@ public class Bacteria extends Organism implements Serializable {
         super();
         BREEDING_AGE = 0;
         MAX_AGE = 5;
-        BREEDING_PROBABILITY = 0.275;
+        BREEDING_PROBABILITY = 0.25;
         LITTER_SIZE = 1;
         rand = new Random();
         this.isResistant = isResistant;
